2eac50a87e5fa19bdadc29ab89cbdfa95888ac38
Ivan <Ivan@DESKTOP-LCTUROF>
2020-08-08 19:18:17 +0200

    Added fields to modal window

--- src/app/component/modal/modal.component.ts
+++ src/app/component/modal/modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { OdlagalisteOtpadaDTO } from '../../model/OdlagalisteOtpadaDTO';
 
 @Component({
   selector: 'app-modal',
@@ -10,14 +11,15 @@ export class ModalComponent implements OnInit {
   
   constructor(private modalService: NgbModal) { }
 
-  @Input() buttonText;
+  @Input() title;
+  @Input() odlagalisteOtpadaDTO: OdlagalisteOtpadaDTO;
 
   ngOnInit(): void {
   }
 
   open(content) {
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'})
-    //.result.then((result) => {});
+    // .result.then((result) => {});
   }
 
 }
